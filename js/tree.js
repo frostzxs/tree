399e99789aedf2598bf2fdfe67a19756dbb4edfa
frostzxs <frostzxs@gmail.com>
2015-05-28 00:06:37 +0500

    оптимизировал код представления сворачивания и выбора флажков

--- js/tree.js
+++ js/tree.js
@@ -19,7 +19,6 @@ $(document).ready(function() {
         $.elTreeCheckboxClick($(this).parent().parent(".elTree").attr('id').substr(2));
     });	
 	
-	
 });
 /*************************************************************/
 
@@ -27,12 +26,18 @@ $(document).ready(function() {
 /**читаем json, парсим в объект и передаем на отрисовку*/
 $.documentReady = function() {
 	var templ = '{"1":{"name":"уровень1","parent":"0","expand":"-","state":"selected"},'+
-	            ' "2":{"name":"уровень2","parent":"1","expand":"","state":"focused"},'+
-	            ' "3":{"name":"уровень1","parent":"0","expand":"" ,"state":""}}';
+	            ' "2":{"name":"уровень2","parent":"1","expand":"-","state":"focused"},'+
+	            ' "3":{"name":"уровень3","parent":"2","expand":"+" ,"state":""},'+
+	            ' "4":{"name":"уровень4","parent":"3","expand":"" ,"state":""},'+
+	            ' "5":{"name":"уровень4","parent":"3","expand":"" ,"state":""},'+
+	            ' "6":{"name":"уровень4","parent":"3","expand":"" ,"state":""},'+
+	            ' "7":{"name":"уровень4","parent":"3","expand":"" ,"state":""},'+
+	            ' "8":{"name":"уровень4","parent":"3","expand":"" ,"state":""},'+	            
+	            ' "9":{"name":"уровень3","parent":"2","expand":"" ,"state":""}}';
 	var tree = jQuery.parseJSON(templ);
 	$.treeDraw(tree);
 
-	/**даем команду на отображение свертывания/развертывания*/
+	/**фиксируем свертывание/развертывание в модели и даем команду на отображение свертывания/развертывания*/
 	$.elTreeExpanderClick = function(id) {
 		switch (tree[id].expand ) {
 		case "+":
@@ -44,7 +49,7 @@ $.documentReady = function() {
 		}
 		$.elTreeToggleDraw(id);
 	}
-	
+	/**фиксируем выбор в модели и даем команду на отображение выбора(переключение флажка элемента)*/
 	$.elTreeCheckboxClick = function(id) {
 	    switch (tree[id].state ) {
         case "selected":
@@ -60,18 +65,20 @@ $.documentReady = function() {
 /*************************************************************/
 
 /**Представление*/
-/**Отрисовка дерева*/
-$.treeDraw = function(tree) {
+/**Отрисовка дерева из модели*/
+$.treeDraw = function(tree) {	
 	var divElTree;
 	var divExpand;
 	var divState;
 	var divCollapse;
 	var inputHighlight;
-
-	$.drawTree = function(parentId) {
-		var p;
-
+	var iter = 0;
+	var p;
+//дальше идет рекурсивно: функция рисует все подэлементы у указанной ветки, затем рисует подэлементы у каждого потомка ветки и т.д.   
+	$.drawTree = function(parentId) {	
+		
 		$.each(tree, function(elementId, value) {
+			iter++;
 			if (tree[elementId].parent == parentId) {
 
 				switch (tree[elementId].expand) {
@@ -112,40 +119,27 @@ $.treeDraw = function(tree) {
                                     '</span>'+
                                 '</div>'+
                             '</div>';
-				$("#id" + tree[elementId].parent).append(divElTree);
+				$("#id" + tree[elementId].parent).append(divElTree);		
 				$.drawTree(elementId);
-			}
+			}			
 		})
 	}
 	$.drawTree("0");
-	alert('готов!');
+	alert('готов!'+iter);
 }
 
 /**отображение свертывания/развертывания*/
 $.elTreeToggleDraw = function(id) {
-	var selector = $('#id' + id).children('.input-group').children('.input-group-addon.action').children('.elTreeExpander');
+	//ищем кнопочку + или -,скрываем вложенные элементы, меняем кнопочку +/-	
+	var selector = $('#id' + id).children('.input-group').find('.elTreeExpander');
 	$('#id' + id).children(".elTree").collapse('toggle');
-
-	/** найти подэлементы, если нашел, то, если показаны подэлементы, то показать -, если не показаны, то показать +
-	 */
-	if (selector.hasClass("glyphicon glyphicon-plus"))
-		selector.removeClass("glyphicon glyphicon-plus").addClass("glyphicon glyphicon-minus")
-	else if (selector.hasClass("glyphicon glyphicon-minus"))
-		selector.removeClass("glyphicon glyphicon-minus").addClass("glyphicon glyphicon-plus");
-}
+	if ((selector.hasClass("glyphicon-plus")) || (selector.hasClass("glyphicon-minus")))
+		selector.toggleClass("glyphicon-plus").toggleClass("glyphicon-minus");
+	}
 
 /**отображение подсветки выбранного элемента*/
-
 $.elTreeSelectDraw = function(id) {
-    var selector = $('#id' + id).children('.input-group').children('.input-group-addon.elCheckbox').children('span.glyphicon');
-        
-    if (selector.hasClass("glyphicon glyphicon-check")) {
-        selector.removeClass("glyphicon glyphicon-check").addClass("glyphicon glyphicon-unchecked");
-        $('#id' + id).children('.input-group').children('.form-control').removeClass('elTreeSelectedHighlight');
-    } else if (selector.hasClass("glyphicon glyphicon-unchecked")) {
-        selector.removeClass("glyphicon glyphicon-unchecked").addClass("glyphicon glyphicon-check");
-        $('#id' + id).children('.input-group').children('.form-control').addClass('elTreeSelectedHighlight');
-    }
-
+    $('#id' + id).children('.input-group').children('.input-group-addon.elCheckbox').children('span.glyphicon').toggleClass("glyphicon-check").toggleClass("glyphicon-unchecked");
+    $('#id' + id).children('.input-group').children('.form-control').toggleClass('elTreeSelectedHighlight');       
 }
 
